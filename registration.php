208da9d61327ad34d70a336fcabc0c90f0659eb9
ViktoriyaVMCG <43714208+ViktoriyaVMCG@users.noreply.github.com>
2019-08-21 03:14:06 -0400

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xml:lang="en" lang="en">
+
+<head>
+  <title>Form Data</title>
+</head>
+<body>
+
+<h1>Form input values</h1>
+<p>Full Name: <?php print $_POST["fullName"]?></p>
+<p>Email: <?php print $_POST["email"] ?></p>
+<p>Password: <?php print $_POST["password"] ?></p>
+<p>Confirm Password: <?php print $_POST["passwordConfirm"] ?></p>
+
+
+</body>
+</html>
